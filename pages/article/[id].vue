b89f44a98fe21b81f76190aebb933d689cae839b
Amiran <chicobes1@gmail.com>
2023-08-27 19:57:51 +0300

    create dinamic routes

---
+++ pages/article/[id].vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<p>{{ $route.params.id }}</p>
+	</div>
+</template>
+
+<script setup>
+import {useRoute} from "#app";
+
+const route = useRoute();
+
+if (!route.params.id) {
+  console.log('Warning! Make sure user is authenticated!')
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
