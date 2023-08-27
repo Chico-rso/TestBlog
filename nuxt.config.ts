ee1371c0f9074701b4e1027c4768550f15674814
Amiran <chicobes1@gmail.com>
2023-08-27 19:44:42 +0300

    init project

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	devtools: {enabled: true},
+	head: {
+		title: 'Test Blog',
+		htmlAttrs: {
+			lang: 'ru',
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width,initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+		],
+	},
+	css: ['@/assets/scss/main.scss'],
+})
---
+++ pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h1>Index page</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae delectus est facilis itaque laboriosam neque optio possimus quisquam velit voluptates?</p>
+	</div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+p
+{
+/* Body 1 (Italic) */
+font-size: 18px;
+font-style: italic;
+font-weight: 300;
+line-height: 26px; /* 144.444% */
+}
+</style>
