1157ac1bc6f6f6bc983141437662ceb73c13e8ff
Amiran <chicobes1@gmail.com>
2026-02-08 18:25:08 +0300

    add yandex metrika

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,38 @@ export default defineNuxtConfig({
                 {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png'},
                 {rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png'},
                 {rel: 'manifest', href: '/site.webmanifest'}
+            ],
+            script: [
+                {
+                    key: 'yandex-metrika',
+                    type: 'text/javascript',
+                    innerHTML: `
+(function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106719755', 'ym');
+
+ym(106719755, 'init', {
+    ssr: true,
+    webvisor: true,
+    clickmap: true,
+    ecommerce: "dataLayer",
+    referrer: document.referrer,
+    url: location.href,
+    accurateTrackBounce: true,
+    trackLinks: true
+});
+                    `.trim()
+                }
+            ],
+            noscript: [
+                {
+                    key: 'yandex-metrika-noscript',
+                    tagPosition: 'bodyOpen',
+                    innerHTML: '<div><img src="https://mc.yandex.ru/watch/106719755" style="position:absolute; left:-9999px;" alt="" /></div>'
+                }
             ]
         }
     },
